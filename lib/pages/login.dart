c5a27c043407297c79c525737427b067cc2febbc
LucianoFraga27 <luciano.lucas@bllc.com.br>
2022-07-26 16:29:52 -0300

    Validator - verificando se o formulário está vazio

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  final _tLogin = TextEditingController();
-  final _tSenha = TextEditingController();
+  final _tLogin = TextEditingController(text: 'usuario');
+  final _tSenha = TextEditingController(text: '123');
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -11,32 +12,45 @@ class LoginPage extends StatelessWidget {
         title: Text('Carros'),
         centerTitle: true,
       ),
-      body: _body(),
+      body: _body(context),
     );
   }
 
-  _body() {
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: ListView(
-        children: [
-          _text('Login', 'Digite o login', controller: _tLogin),
-          SizedBox(
-            height: 10,
-          ),
-          _text('Senha', 'Digite a senha', obscure: true, controller: _tSenha),
-          SizedBox(
-            height: 20,
-          ),
-          _button('Login', _onClickLogin)
-        ],
+  _body(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: ListView(
+          children: [
+            _text('Login', 'Digite o login',
+                controller: _tLogin, validator: _validateLogin),
+            SizedBox(
+              height: 10,
+            ),
+            _text('Senha', 'Digite a senha',
+                obscure: true, controller: _tSenha, validator: _validateSenha),
+            SizedBox(
+              height: 20,
+            ),
+            _button('Login', () {
+              return _onClickLogin(context);
+            })
+          ],
+        ),
       ),
     );
   }
 
-  TextFormField _text(String label, String hint,
-      {bool obscure = false, required TextEditingController controller}) {
+  TextFormField _text(
+    String label,
+    String hint, {
+    bool obscure = false,
+    required TextEditingController controller,
+    required FormFieldValidator<String> validator,
+  }) {
     return TextFormField(
+      validator: validator,
       controller: controller,
       obscureText: obscure,
       style: TextStyle(fontSize: 20, color: Colors.blue),
@@ -64,9 +78,29 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  _onClickLogin() {
+  _onClickLogin(BuildContext context) {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
     String login = _tLogin.text;
     String senha = _tSenha.text;
     print("Login: $login | Senha: $senha");
+  }
+
+  String? _validateLogin(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Digite o login';
+    }
+    return null;
+  }
+
+  String? _validateSenha(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Digite a senha';
+    }
+    if (value.length < 3) {
+      return 'A senha precisa ter pelo menos 3 caracteres';
+    }
   }
 }
