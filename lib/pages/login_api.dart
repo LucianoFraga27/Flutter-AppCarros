da26224305cb7657279a196017e23486c7ffb37f
LucianoFraga27 <luciano.lucas@bllc.com.br>
2022-07-28 13:20:23 -0300

    Parser de JSON (simples)

--- lib/pages/login_api.dart
+++ lib/pages/login_api.dart
@@ -24,6 +24,13 @@ class LoginApi{
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
 
+    Map mapResponse = json.decode(response.body);
+
+    String nome = mapResponse["nome"];
+    String email = mapResponse["email"];
+
+    print("Nome: $nome");
+    print("Email: $email");
     return true;
   }
 }
