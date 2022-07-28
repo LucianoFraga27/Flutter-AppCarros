b696e29c3d0fe4d2a986033176193dada3a23ca2
LucianoFraga27 <luciano.lucas@bllc.com.br>
2022-07-27 22:52:50 -0300

    Login via WebService - add=login_api.dart

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 import '../utilis/nav.dart';
 import 'home_page.dart';
+import 'login_api.dart';
 
 class LoginPage extends StatefulWidget {
 
@@ -12,9 +13,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final _tLogin = TextEditingController(text: 'usuario');
+  final _tLogin = TextEditingController(text: "admin");
 
-  final _tSenha = TextEditingController(text: '123');
+  final _tSenha = TextEditingController(text: "123");
 
   final _formKey = GlobalKey<FormState>();
 
@@ -64,15 +65,24 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  _onClickLogin(BuildContext context) {
+  _onClickLogin(BuildContext context) async {
     if (!_formKey.currentState!.validate()) {
       return;
     } else {
+
       String login = _tLogin.text;
       String senha = _tSenha.text;
+
       print("Login: $login | Senha: $senha");
 
-      push(context, HomePage());
+      bool ok = await LoginApi.login(login,senha);
+
+      if(ok) {
+        push(context, HomePage());
+      } else{
+        print('Login incorreto!');
+      }
+
     }
 
     }
@@ -88,8 +98,8 @@ class _LoginPageState extends State<LoginPage> {
     if (value == null || value.isEmpty) {
       return 'Digite a senha';
     }
-    if (value.length < 3) {
-      return 'A senha precisa ter pelo menos 4 caracteres';
+    if (value.length > 3) {
+      return 'A senha precisa ter pelo menos 3 caracteres';
     }
   }
 }
